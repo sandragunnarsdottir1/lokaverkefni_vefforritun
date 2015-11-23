c4c1aac8b5e00c5cd0f2e9f6ed738b4520529ae9
sandragunnarsdottir1 <sandragunnarsdottir1@gmail.com>
2015-11-23 13:27:53 +0000

    sækja martins

--- routes/forum.js
+++ routes/forum.js
@@ -4,11 +4,15 @@ var express = require('express');
 var router = express.Router();
 var dbUtils = require('../utils/db-utils');
 
+var idhelp = 0;
+
 router.get('/', function(req, res) {
-	console.log(req.query.id);
+	console.log('fyrirspurn: ' + req.query.id);
 	var titleid = req.query.id;
-	var queryStr2 = 'SELECT * FROM entries WHERE title = title'
-	dbUtils.queryDb(queryStr2, null, function(err, result) {
+	idhelp = titleid;
+	var queryStr2 = 'SELECT * FROM entries WHERE titleid = $1'
+	var parameters = [titleid];
+	dbUtils.queryDb(queryStr2, parameters, function(err, result) {
 	if(err) {
 	  return console.error('error fetching client from pool', err);
 	}
@@ -27,4 +31,26 @@ router.get('/', function(req, res) {
 	});*/
 });
 
+
+
+router.post('/blog', function(req, res) {
+  var commentid = idhelp;
+  console.log('commentid er: ' + commentid);
+  var queryStr = 'INSERT INTO entries' + 
+  ' (entry_text, username, create_date, titleid) ' +
+  'VALUES ($1, $2, now(), $3)';
+  var parameters = [req.body.entry,req.session.user.username,commentid]; 
+  dbUtils.queryDb(queryStr, parameters, function(err) {
+    if(err) {
+      console.log('tókst ekki að gera færslu')
+      return console.error('error fetching client from pool', err);
+    }
+    console.log('tókst að gera færlsuna');
+    res.redirect('/forum?id='+commentid);
+  });   
+    
+});
+
 module.exports = router;
+
+
--- routes/register.js
+++ routes/register.js
@@ -16,8 +16,8 @@ router.post('/', function(req, res) {
   
   var hashedPassword = passwordHash.generate(req.body.password);
   var queryStr = 'INSERT INTO users '+
-  '(name, username, password) '+
-  'VALUES ($1,$2,$3)';
+  '(name, username, password, email, emaillist, gender, dateusers) '+
+  'VALUES ($1,$2,$3,$4,$5,$6,$7)';
   var parameters = [req.body.name, req.body.username, hashedPassword];
   dbUtils.queryDb(queryStr, parameters, function(err) {
     if(err) {
